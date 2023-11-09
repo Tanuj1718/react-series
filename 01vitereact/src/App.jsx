af9700bc2ed78196df9e8ae7b18723c9991a515c
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2023-11-09 20:47:48 +0530

    Changes after React03

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Chai from "./chai"
+function App() {
+
+  return (
+    <>
+    <h1>chai aur react|| tanuj</h1>
+    <Chai/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,9 @@
+function Chai(){
+    return (
+        <>
+        <h1>Chai with react</h1>
+        <p>Lets drink tea man</p>
+        </>
+    )
+}
+export default Chai
