2f76002a199cb03d39cc5bb22a4fe256ed10ab23
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2023-12-26 18:39:54 +0530

    React Router

--- CurrencyConvertor/src/components/InputBox.jsx
+++ CurrencyConvertor/src/components/InputBox.jsx
@@ -10,8 +10,6 @@ function InputBox({
     amountDisable = false,
     currencyDisable = false,
     className = "",
-    
-    className = "",
 }) {
    
 
@@ -35,10 +33,16 @@ function InputBox({
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e)=> onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
                 > 
-                        <option value="usd">
-                            usd
-                        </option>
+                {currencyOption.map((currency)=>{
+                     <option key={currency} value={currency}>
+                     {currency}
+                 </option>
+                })}
+                       
                 
                 </select>
             </div>
