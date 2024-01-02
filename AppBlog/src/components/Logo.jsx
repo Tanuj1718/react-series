4f55c20f9de0595673b0ab7ed9ab3323559bb041
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-01-02 13:42:39 +0530

    Blog App Project

--- AppBlog/src/components/Logo.jsx
+++ AppBlog/src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
-    <div>Logo</div>
+    <div className='w-12'><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAwMld3i935CBg7u6oqIfzttme3Bq09WDwGVNjNlTEv38HpajEkglfLa_U7HA1THJzbx0&usqp=CAU" alt="Logo" /></div>
   )
 }
 
--- AppBlog/src/pages/Home.jsx
+++ AppBlog/src/pages/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Login to read posts
+                                To see posts, Please Login!
                             </h1>
                         </div>
                     </div>
